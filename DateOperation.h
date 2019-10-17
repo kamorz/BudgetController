68293bc4b4f86897e16609d58cd90898802ed843
Kamil Orzechowski <kamil.orzechowski.programista@gmail.com>
2019-10-17 18:48:55 +0200

    Added new class DateOperation

---
+++ DateOperation.h
@@ -0,0 +1,21 @@
+#ifndef DATEOPERATION_H
+#define DATEOPERATION_H
+
+#include <iostream>
+#include <sstream>
+#include <windows.h>
+#include <winbase.h>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+
+class DateOperation
+{
+
+public:
+
+
+
+};
+
+#endif
